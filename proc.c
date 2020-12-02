20722cb42f3fbd14f02ffb4835b9124c80e9527a
arvindh75 <iiith@arvindharun.com>
2020-12-02 11:11:23 +0530

    Fixed set_priority bug

--- proc.c
+++ proc.c
@@ -439,8 +439,8 @@ int set_priority(int new_priority, int pid){
     struct proc *p;
     acquire(&ptable.lock);
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-        if(p->pid != pid)
-            continue;
+        if(p->pid == pid)
+            break;
     }
     old_priority = p->priority;
     p->priority = new_priority;
